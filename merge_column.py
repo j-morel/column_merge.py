a3f0d6f8f836bbad4591fbe530eeebd962b47e98
Jack Morel <jmorel@riffyn.com>
2017-06-29 16:53:19 -0700

    Clean Up
    Removed original debugging tools and print statements.

--- merge_column.py
+++ merge_column.py
@@ -4,41 +4,31 @@ import numpy as np
 from fuzzywuzzy import fuzz
 import copy
 
-#Use fuzzy logic for simplicity
-#Print functions used to visualize merging events
 df=copy.copy(test_data)
-#print(df)
+
 def column_merge(df,thresh=None,start_point=0):
     """
     Add columns together based on similarity of column names
-
     Parameters
     ----------
     df : pandas dataframe object
         A dataframe object with at least 2 columns and 1 row
     thresh : int, default=60
         Matching threshhold between 0 and 100. Listed as none to allow for update in each recursive call
-
     Returns
     -------
     DataFrame: with merged column name
     """
     thresh=60
     size=len(df.columns)-1
-
     for i in range(start_point,size):
         if i+1==len(df.columns):
             break
         elif fuzz.ratio(df.columns[i],df.columns[i+1])>thresh:
             df.iloc[:,i]+=df.iloc[:,i+1]
-            #print(">>column merged at %i and %i with fuzz = %i > %i"%(i,i+1,fuzz.ratio(df.columns[i],df.columns[i+1]),thresh))
             df.drop(df.columns[i+1],axis=1,inplace=True)
-            #print(df)
             column_merge(df,start_point)
         else:
             0
         start_point+=1
-        #print(">>column skipped at %i and %i with fuzz = %i < %i"%(i,i+1,fuzz.ratio(df.columns[i],df.columns[i+1]),thresh))
-        #print(df)
-
     return df
